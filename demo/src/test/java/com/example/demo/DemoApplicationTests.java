58b6f50b814f961457d23615d03bfd6c23b658f9
zhaoyansheng163 <zhaoyansheng163@163.com>
2023-05-28 20:20:36 +0800

    1、增加通过单元测试添加任务 2、修复页面因为引用的vue.js失效 导致页面打不开的问题

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.job.HelloJob;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
-
+	@Autowired
+	@Qualifier("Scheduler")
+	Scheduler scheduler;
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws SchedulerException {
+		JobDetail jobDetail = JobBuilder.newJob(HelloJob.class).withDescription("with HelloJob in HelloJob job init....").withIdentity("HelloJob", "groupA").build();
+		CronTrigger cronTrigger = TriggerBuilder.newTrigger().withSchedule(CronScheduleBuilder.cronSchedule("0 0/2 * * * ?")).build();
+		scheduler.scheduleJob(jobDetail,cronTrigger);
+		scheduler.start();
 	}
 
 }
